870af9fffbaaa886e8c4a46f3846590577c48c80
greenfructose <justinclarkturney@gmail.com>
2020-12-10 16:04:57 -0600

    Finished s&p500 equal weight portfolio

--- sp500-eqweight.py
+++ sp500-eqweight.py
@@ -3,5 +3,108 @@ import pandas as pd
 import requests
 import xlsxwriter
 import math
+from secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
+symbol = 'AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+price = data['latestPrice']
+market_cap = data['marketCap']
+
+columns = ['Ticker', 'Stock Price', 'Market Capitalization', 'Number of Shares to Buy']
+# df = pd.DataFrame(columns=columns)
+# for stock in stocks['Ticker']:
+#     api_url = f'https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_CLOUD_API_TOKEN}'
+#     data = requests.get(api_url).json()
+#     price = data['latestPrice']
+#     market_cap = data['marketCap']
+#     df = df.append(
+#         pd.Series(
+#             [
+#                 stock,
+#                 price,
+#                 market_cap,
+#                 'N/A'
+#             ],
+#             index=columns
+#         ),
+#         ignore_index=True
+#     )
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from list"""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+
+df = pd.DataFrame(columns=columns)
+for symbol_string in symbol_strings:
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        df = df.append(
+            pd.Series(
+                [
+                    symbol,
+                    data[symbol]['quote']['latestPrice'],
+                    data[symbol]['quote']['marketCap'],
+                    'N/A'
+                ], index=columns
+            ), ignore_index=True
+        )
+portfolio_size = input('Enter the value of your portfolio:')
+try:
+    val = float(portfolio_size)
+except ValueError:
+    print('Please enter a number')
+    portfolio_size = input('Enter the value of your portfolio:')
+    val = float(portfolio_size)
+
+position_size = val/len(df.index)
+for i in range(0, len(df.index)):
+    df.loc[i, 'Number of Shares to Buy'] = math.floor(position_size/df.loc[i, 'Stock Price'])
+
+writer = pd.ExcelWriter('sp500-eqweight-trades.xlsx', engine='xlsxwriter')
+df.to_excel(writer, 'Trades', index=False)
+background_color = '#0a0a23'
+font_color = '#ffffff'
+string_format = writer.book.add_format(
+    {
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1
+    }
+)
+dollar_format = writer.book.add_format(
+    {
+        'num_format': '$0.00',
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1
+    }
+)
+integer_format = writer.book.add_format(
+    {
+        'num_format': '0',
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1
+    }
+)
+column_formats = {
+    'A': ['Ticker', string_format],
+    'B': ['Stock Price', dollar_format],
+    'C': ['Market Capitalization', dollar_format],
+    'D': ['Number of Shares to Buy', integer_format]
+}
+for column in column_formats.keys():
+    writer.sheets['Trades'].set_column(f'{column}:{column}', 18, column_formats[column][1])
+    writer.sheets['Trades'].write(f'{column}1', column_formats[column][0], column_formats[column][1])
+writer.save()
